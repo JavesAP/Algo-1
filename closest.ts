dc2c38cb86a250b5504c67ad4e3d6ce202e1d072
Javes Hernandez <javesap3@gmail.com>
2024-09-16 18:44:02 -0400

    add solution

--- closest.ts
+++ closest.ts
@@ -1,6 +1,18 @@
 export function closest(nums: number[], target: number): number {
-   // Implement me
-   return 0;
+   let closestSum = Number.MAX_VALUE;
+
+    for(let a = 0; a < nums.length ; a++) {
+        for(let b = a + 1; b < nums.length; b++) {
+            for(let c = b + 1; c < nums.length; c++) {
+               
+               if ((Math.abs(target - closestSum) > Math.abs(target - (nums[a] + nums[b] + nums[c])))) {
+                 closestSum = (nums[a] + nums[b] + nums[c]);
+               }
+
+            } 
+        }
+    }
+   return closestSum;
 }
 
 module.exports = { closest }
